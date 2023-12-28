109504a6b39638e1d48e1054ecf282057e2b4b5a
Daniel Milner <danielmmilner@gmail.com>
2023-12-28 16:42:29 +1100

    Day 9

--- Tests/Tests/Day9Test.cs
+++ Tests/Tests/Day9Test.cs
@@ -5,14 +5,14 @@ namespace Tests.Tests;
 public class Day9Test : DayTest
 {
     [Fact]
-    public void SamplePart1() => Assert.Equal(-1, SamplePart1Answer);
+    public void SamplePart1() => Assert.Equal(114, SamplePart1Answer);
 
     [Fact]
-    public void Part1() => Assert.Equal(-1, Part1Answer);
+    public void Part1() => Assert.Equal(1995001648, Part1Answer);
 
     [Fact]
-    public void SamplePart2() => Assert.Equal(-1, SamplePart2Answer);
+    public void SamplePart2() => Assert.Equal(2, SamplePart2Answer);
 
     [Fact]
-    public void Part2() => Assert.Equal(-1, Part2Answer);
+    public void Part2() => Assert.Equal(988, Part2Answer);
 }
